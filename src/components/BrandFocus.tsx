1db79f7d1e82ffa9c10c2f3118e881d296ca7276
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-10-29 13:33:54 +0900

    components/BrandFocus 완성

--- src/components/BrandFocus.tsx
+++ src/components/BrandFocus.tsx
@@ -7,16 +7,21 @@ function BrandFocus() {
         <div className="text-[20px] font-semibold tracking-tighter">
           Brand Focus
         </div>
-        <div className="text-[12px] text-gray-400">추천 브랜드</div>
-      </div>
-      <div className="h-[416px]">
-        {/* map 함수가 return값이 필요해서 return 해와야 된다라고 오인 */}
-        {BrandArray.map((item, i) => (
-          <div>
-            <img src={item?.ImageUrl} />
-            {item?.name}
-          </div>
-        ))}
+        <div className=" text-[12px] text-gray-400">추천 브랜드</div>
+        <div className="h-[416px] mt-[1rem] ml-[8px] grid grid-cols-5">
+          {/* map 함수가 return값이 필요해서 return 해와야 된다라고 오인 */}
+          {BrandArray1.map((item, i) => (
+            <div>
+              <img
+                src={item?.ImageUrl}
+                className="w-[228px] ml-[4px] mr-[4px]"
+              />
+              <div className="mt-[8px] mb-[8px] text-[15px] flex justify-center">
+                {item?.name}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -29,83 +34,83 @@ interface Brand {
   ImageUrl: string;
 }
 
-const BrandArray: Array<Brand | undefined> = [
+const BrandArray1: Array<Brand | undefined> = [
   {
     name: "애플",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTQ4/MDAxNjYxMTMyMzQ2MTQ3.57ah96yqHitKYQNruBLxBS901N1LfgNg8TKpEfGSGR8g.oJWA3A7Ng2OU83DHm3WqrIOZduVPdFfPVPfWAqB4jd0g.JPEG/a_9f657db2a3884f86a5ad98c8017d6537.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfNTMg/MDAxNjYxMTMyMzQ4Njg5.RsvtKwAj6FHj1FDDL5TY1ycECnCPB12EttYPY5M1gBsg.vpRsBYsFuP-GSd4ODRJe2P4TkiVFE9Buu6Yj777Kesog.JPEG/a_d8ca5e787c1a47f086c405177abc9694.jpg?type=m_webp",
   },
   {
     name: "루이비통",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA5MzBfMTE3/MDAxNjY0NTM2NDQzOTY1.fSCxmH0HmxenIoi4pXlZI5LXGheCABvLnT4PzUt7Vukg.JT-Gg69GaMkOqrdvh4CJi1uynYcjB3A-oy0I5RQpbMcg.JPEG/a_0a447621d64345599c5ff11d1466a4c5.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA5MzBfMTM2/MDAxNjY0NTM2NDQ2MzQ5.kC1qMK6mltl0PvSNqWiq3KroRn067Mbae-OzlEnwMEQg.jcAazVYaMq-ZB6fewYkisfqBQIIK6n6L59h_1PpKwaUg.JPEG/a_8a407a9e001a425ab4c10864c6fd273d.jpg?type=m_webp",
   },
   {
     name: "강혁",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjEwMDdfMjI1/MDAxNjY1MTM0ODc3MTA0.A2KTSd_P8lKFY3FmmLKtuAj4Ow5qiDzW0lUqBL4WIxsg.2PzErgC4HI09cyyG8hL0xmQUddnNSA9tSPdasu_xV8wg.JPEG/a_fe3fd603e5604ac6bc904476864d706b.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjEwMDdfMTQ2/MDAxNjY1MTM0ODc5Mzcx.hWLeBlp0yyEWGq6goJeMLPoSZTTgufx7_2wLDPvOLeYg.xycA1YUscG0NS63oPgVIpxoMhLt_2z-n_Xz4zqApRE8g.JPEG/a_e9f65e8566144b8e8aaf25ab788969dd.jpg?type=m_webp",
   },
   {
     name: "우영미",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTIy/MDAxNjYxMTMyNTE0ODk3.1MmBTlJmvqRzE1zCNC7RccFGlzjSeU8ZJDRiwaraE9cg.jwXESgD2cUNrjWKhdm5KC63uMEC5jQGL-nc4PIJ94QAg.JPEG/a_c99ccda34c8b4e218b4c4721db5ac476.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMjUy/MDAxNjYxMTMyNTE3NDkw.WBGPz7YTPljlTjBVVGNKHY8a0UohliA2ahbwJmqF4rAg.4QYXLQna4D-CHul0MSeru4vP1dcn-qRVkPDuezT3_5sg.JPEG/a_5f636bae775f4b5f8538d969e6916238.jpg?type=m_webp",
   },
   {
     name: "에르메스",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMjYz/MDAxNjYxMTMyMjczNjkz.0k9rKUtR-nt95SmpMEeARI8GgQNUBowBoonJz_Par-sg.62zpHkqPIpO2yAb7iFe5n4t-o8HXP7m14BqiQnolbD0g.JPEG/a_40a13dde8e904b948fd024c514f5c0ab.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTU2/MDAxNjYxMTMyMjc3ODYy.o_bZ8NwB1fDxBycvBadiwSjJ3sJrAfNcgzHXZO3SCskg.U9-45gdxEevSSwnM8xAHqDBdIsBhbR4TCwK3xrBmMaIg.JPEG/a_41ee4f97c403419784fe145cbfe9e950.jpg?type=m_webp",
   },
   {
     name: "발렌시아가",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfODYg/MDAxNjYxMTMyNDk4Njky.PCV4xjVti0lCwGtAZN3FodKL9nE5_I5l5gR8xe_7KPgg.LDmzaPnQ2TQhmnrdyAhtVqK3yOYQ8eMvPCioiiSyi_Ag.JPEG/a_2b6a2ac11f4c40ad9225ab43cfbbcbcf.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMjgw/MDAxNjYxMTMyNTAwODI0.uvibwIpVXKhBYiNd5gCCRH-W0qk-RX3J1FXF58o69nMg.aa6DHYk0_42QwB5scZ2taZL8WGKmfkcMlOMWl9AV5z0g.JPEG/a_4ed967e942554a14a5687b0d527bbe58.jpg?type=m_webp",
   },
   {
     name: "아크테릭스",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA5MzBfNTUg/MDAxNjY0NTM2NzM0NTIz.KFaQp9HDcETLZZKMgITasYc-JZIj7WyMyYeMtKfkF6Mg.pyvkrZFPNkSP8tIoQuDzsG8E1DWpUg7LYp1cdqsxxqIg.JPEG/a_3881b1ae706047d791cb9eb540fb639a.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA5MzBfMjMz/MDAxNjY0NTM2NzM2Mzc1.6w2bcZkBccLv112TzFBOvWKzBsx8qE4A4Q4ODq5uz6Ig.qOOuTt5_Ch5khC3ODXUGnh1s1ErTwxg42w4fG2lzzDAg.JPEG/a_54af49e527524cdc98db28d6fb1ffade.jpg?type=m_webp",
   },
   {
     name: "렉토",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjEwMDdfMjA0/MDAxNjY1MTM0ODM2NDQ4.WiZqgw5QwKqDB69LekX2s5PIkxwr6nkb1cLQ0UBy9Jwg.eHbkyujtHVIDzKrBWu35lXtl-9kiY9YDA3RAGQPs16og.JPEG/a_ab332a20fbff4168a2f85f11c97a8f88.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjEwMDdfMTEx/MDAxNjY1MTM0ODM4MDMw.HzVvSOi98fjQmP3oroAmNe2sR32FZDFbobzy7tjpIZ4g.reteIbS3HkHf-UxIRYF-BIHFxCpH1JrRcEFSmcJVxfMg.JPEG/a_02632df7a3a049e7b6037802a5a206b4.jpg?type=m_webp",
   },
   {
     name: "샤넬",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTI2/MDAxNjYxMTMyMjY0MDQ3.5lQaaA7L8VQ70H-A239Zfl-W265UUs3BX_KogTVTfg0g.7AvkIA1S0_T3Pt9JQrrCzebnqe52XLN5ksk34NffK1Ug.JPEG/a_d46f1d11219c4aff82bf371df1f2902c.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfODMg/MDAxNjYxMTMyMjY2NDU0.veNBMpvgteBr5I5SLuqNPHR8CkwWhEytMWZoG735frkg.PerHHfgDwBfbVU1BIwEXChh2_-w8u261iDgwUJiCp54g.JPEG/a_e37129ba9c134102b8f886ee9cfc5b57.jpg?type=m_webp",
   },
   {
     name: "디올",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTAz/MDAxNjYxMTMyMzA5NzMx.3_6H5Bw8Op9rSpFZlqelXF-N5qerbWnSqNjjLd5w9nEg.uK1eMkcPs2DUzwHdIdjBFjz2t8f_JeHKfm3lNfn7t5Ug.JPEG/a_4defacefcceb4808a1b66df22219d02e.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMjM2/MDAxNjYxMTMyMzE0NTg5.f8aopPNBfz2TiMXJPHCA4cDSE564M-5m_1CVOqBaMJcg.LP8fIRuj3KwekpzWqPNM1E7udU5MgzCeyeVG5hL8oFog.JPEG/a_dd2fc85edf3b45e18d1c1b83180c004c.jpg?type=m_webp",
   },
   {
     name: "스톤 아일랜드",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMyAg/MDAxNjYxMTMyNDg2MDAy.sGW1BWQdpd2olupBqUmmb4cdKSKQoBQBUPWoaT_wxZUg.VkvY4tbMJyQNXJGuTNGJHGCUga_b1lvyNE7XA2PPWk0g.JPEG/a_2be3f8f62a5c4dcfb5f9ccdcd192701f.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTgg/MDAxNjYxMTMyNDg5MzI4.91XVGjgCI_MCIl_wqButgXZUy7kdk_yXjOn2kg8y6AEg.dyIleHyozgiFkXvyk8pAn0SUAuz2IkwxqXxVC1Clb48g.JPEG/a_36de4c2d690d423ea5ba7a876b00ebd3.jpg?type=m_webp",
   },
   {
     name: "롤렉스",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTAg/MDAxNjYxMTMyMzIxMjAx.FZgEJ0P1zyF8-5gu89wV8yLfiLZB5wDrqSIcki475W0g.9Sv3hqnkJfg2JzQktMEwTePvSxa72yxbTf_cUFyNUjog.JPEG/a_ed65aea06d1048bb952dda6682d7528d.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTgy/MDAxNjYxMTMyMzI1ODY2.omWU_zpi2-_4IK9XyTPQFDfqADR6p8yqhK_VdchChFwg.h2JuTtFOH763vLDGZIbMj2HC53joOaME44CbdEjGaA0g.JPEG/a_080b41c445b94290b693aa53a8751fc5.jpg?type=m_webp",
   },
   ,
   {
     name: "보테가 베네타",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTgz/MDAxNjYxMTMyNDEzOTUy.6wL1GyW6WQfP4hAxS8gNPFoQbFxcNsLglb5X8cMe5u0g.9I2T4Z6nkclU1BaQOtD6w3kDqboaZtLZz0etUZa8BkEg.JPEG/a_83edb8576a1b4b499403f0780e921274.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTY2/MDAxNjYxMTMyNDE3MDg1.NH05zJR5hBf3wHvQNhnvKLVxlGLtXBwaLYNuxsXQL78g.B2wHGjWAx_2n6oXfK4gy92vHpu7hbjin0U_XNwa8mlcg.JPEG/a_fe6d110270b4422e9d35b90e66a3235d.jpg?type=m_webp",
   },
   ,
   {
     name: "셀린느",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMjkg/MDAxNjYxMTMyNDI1MTUz.2GdDlqWvJP1B-LN1xomugypiDHyFUCNJy1HJcGRY2TEg.3CelTgH9Nuotchjwt-dLMxJ7qrxQqJPE-uB-UyNosYkg.JPEG/a_cf6eb629ff3b4ceeac1c0226a19dc972.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMTEw/MDAxNjYxMTMyNDI3Njg4.gNnZj-Nb_7BNlNaJ0KMwEoxtp1E3O_PYvNFiwz0USPEg.cVgu4uf4pB24POhPepRYihXTIG146a9_iShSKfCPFnsg.JPEG/a_ecbebc5f0f354369bac78d86766ed990.jpg?type=m_webp",
   },
   ,
   {
     name: "프라다",
     ImageUrl:
-      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfMjUy/MDAxNjYxMTMyNDYyOTQ1.HQuxYR5B1Fp-f338ih9_WjPOWixfAtZXV5lBLN1SCBQg.F3A1zajKzLfR7UV2oJgaVcSUDUCUz-InuMlfBRhFrPIg.JPEG/a_9661d07bb9784440bfbdd008321761ef.jpg_webp",
+      "https://kream-phinf.pstatic.net/MjAyMjA4MjJfNTAg/MDAxNjYxMTMyNDY2ODM1.IYpY8jJHjZTN9g-Yjc9HJ3x7npeZ-0VCrMr4KApSSNMg.a_Wtikd-hRYxM1nImn9s94MVpuzoLz04JPpGOvQM52Mg.JPEG/a_1d7ab41649cc478988f6759f8d6c2bc0.jpg?type=m_webp",
   },
 ];
